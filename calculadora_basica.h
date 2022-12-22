f21063d06e3a1f7f9a9a87f8e24efb024df794e6
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2022-12-22 10:51:48 -0300

    Update calculadora_basica.h

--- calculadora_basica.h
+++ calculadora_basica.h
@@ -1,12 +1,13 @@
-//Este arquivo contém a classe do app da calcculadora
+//Este arquivo contém a classe do app da calculadora
 /*Cabecalho*/
 #include<gtkmm.h>
+#include"calculadora_basica_ajuda.h"
 using namespace std;
 using namespace Glib;
 using namespace Gtk;
 
 
-//Definindo a classe de aplicativos
+//CLASSE DO APP
 
 class calculator: public Window{
 public:
@@ -18,8 +19,8 @@ string display_texto, display_temp;
 long double parcela1, parcela2, resultado, memoria_valor, display_valor, temp;
 bool ad, su, mu, di;
 
-//Widgets (nomes em português)
-//Box
+//WIDGETS
+//CAIXAS
 Box base, caixa1, caixa2;
 //TELA DA CALCULADORA
 Entry display;
@@ -27,15 +28,20 @@ Entry display;
 Grid numeric_grid, arithmetic_grid;
 Frame numeric_frame, arithmetic_frame;
 //BUTTONS
-Button zero, um, dois, tres, quatro, cinco, seis, sete, oito, nove, mais, menos, vezes, dividir, igual, mais_ou_menos, ponto;
-Button limpar, desligar, memoria, memoria_cache, raiz_quadrada;
-
-//Funções úteis (nomes em inglês)
+Button zero, zerozero, um, dois, tres, quatro, cinco, seis, sete, oito, nove, mais, menos, vezes, dividir,  mais_ou_menos, ponto;
+Button limpar, desligar, memoria, memoria_cache, raiz_quadrada, backspace, igual;
+//JANELA DE AJUDA E BARRA DE MENU
+MenuBar help_bar;
+HelpWindow* help_window=new HelpWindow;
+//FUNÇÕES ÚTEIS
 void ce();
 void off();
 void plus_minus();
+void back();
 void equal();
+//FUNÇÕES DE ENTRADAS DE DADOS
 void put_point();
+void put_00();
 void put_0();
 void put_1();
 void put_2();
@@ -46,6 +52,7 @@ void put_6();
 void put_7();
 void put_8();
 void put_9();
+//FUNÇÕES DE OPERADDORES MATEMÁTICOS E MANIPULAÇÃO DE VARIÁVEIS
 void add2();
 void subtract();
 void multiply();
@@ -53,5 +60,6 @@ void divide();
 void memory();
 void memory_clear();
 void square_root();
-
+//AJUDA
+void help();
 };
